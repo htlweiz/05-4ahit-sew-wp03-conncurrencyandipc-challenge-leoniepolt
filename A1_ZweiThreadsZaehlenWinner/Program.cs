bef8465c0fa8f127530c9e35dd743d650ff44cbc
leoniepolt <polt220050@sr.htlweiz.at>
2025-11-10 08:30:51 +0100

    erstes bsp nr 3 fertig

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -1,46 +1,73 @@
 using System;
 using System.Threading;
-
+ 
 namespace A1_ZweiThreadsZaehlenWinner;
-
+ 
 class Program
 {
-   static int countUp = 0;
-   static int countDown = 0;
-    
+ 
+    static int threadACount = 0;
+    static int threadBCount = 0;
+
+    static int number = 0;
+ 
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
-        Thread thread1 = new Thread(() => CountUpThreadA());
-        Thread thread2 = new Thread(() => CountDownThreadB());
-        
-        thread1.Start();
-        thread2.Start();
+        Thread threadA = new Thread(() => CountUpThreadA());
+        Thread threadB = new Thread(() => CountDownThreadB());
+ 
+        threadA.Start();
+        threadB.Start();
+ 
+        threadA.Join();
+        threadB.Join();
 
-        thread1.Join();
-        thread2.Join();
-
-        
+        if(number == 51){
+            Console.WriteLine($"thread up gewinnt - number: {number}");
+        }else if(number == 49){
+            Console.WriteLine($"thread down gewinnt - number: {number}");
+        }
+         
     }
-    
+   
     private static void CountUpThreadA()
     {
-        for(int i = 1; i <= 100; i++)
+        for(int i = 1; i<= 100; i++)
         {
+            threadACount = i;
+            /*if(threadBCount == threadACount)
+            {
+                Console.WriteLine("B:" + threadBCount);
+                Console.WriteLine("A:" + threadACount);
+                break;
+            }*/
+
+            if(i == 51){
+                number = 51;
+            }
             Thread.Sleep(100);
-            countUp = countUp + 1;
-            System.Console.WriteLine(countUp);
         }
+       
     }
-    
+   
     private static void CountDownThreadB()
     {
-       for(int i = 100; i <= 1; i--)
+        for(int i = 100; i>= 1; i--)
         {
+            threadBCount = i;
+            /*if(threadACount == threadBCount)
+            {
+                Console.WriteLine("B:" + threadBCount);
+                Console.WriteLine("A:" + threadACount);
+                break;
+            }*/
+             if(i == 49){
+                number = 49;
+            }
             Thread.Sleep(100);
-            countDown = countDown - 1;
-            System.Console.WriteLine(countDown);
         }
     }
 }
+ 
+ 
