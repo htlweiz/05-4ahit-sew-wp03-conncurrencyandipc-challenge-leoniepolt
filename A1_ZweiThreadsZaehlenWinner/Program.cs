ecefdac2e245175f21724091558fd8504278c3da
leoniepolt <polt220050@sr.htlweiz.at>
2025-11-10 08:02:49 +0100

    01 done

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,22 +5,38 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
+   int countUp = 0;
+   int countDown = 0;
     
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
+        Thread thread1 = new Thread(() => CountUpThreadA());
+        Thread thread2 = new Thread(() => CountDownThreadB());
         
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
     }
     
     private static void CountUpThreadA()
     {
-        
+        for(int i = 1; i <= 100; i++)
+        {
+            Thread.Sleep(100);
+            countUp = countUp + 1;
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+       for(int i = 1; i <= 100; i++)
+        {
+            Thread.Sleep(100);
+            countDown = countDown - 1;
+        }
     }
 }
